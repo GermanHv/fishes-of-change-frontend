bce13ef2c98fff5cea73032fa1f72b3a566fee40
Beto-cpu <beto.munguia.lara@gmail.com>
2022-05-15 11:07:29 -0700

    Clicker store added

--- components/clicker.tsx
+++ components/clicker.tsx
@@ -1,96 +1,178 @@
 import { useEffect, useState, useRef } from "react";
 const oilDrillFigure = '../assets/figures/oil-drill-1.png';
-const oilDrop = "../assets/figures/oil-drop.png"
+const oilDrop1 = "../assets/figures/oil-drop-1.png"
+const fish = "../assets/figures/fish-2.png"
 
-const randomBetween = (x1:number, x2:number) => ( Math.floor(Math.random() * (x2 - x1)) + x1 );
+const randomBetween = (x1: number, x2: number) => (Math.floor(Math.random() * (x2 - x1)) + x1);
+const store = [
+    { price: 5000, id: 0, name: 'Tratar aguas' },
+    { price: 300, id: 1, name: 'Mantenimiento Mensual' },
+    { price: 1800, id: 2, name: 'Mantenimiento Anual' },
+    { price: 50, id: 3, name: 'Mejorar Drill 1 (+1)' },
+    { price: 150, id: 4, name: 'Mejorar Drill 2 (+5)' },
+    { price: 400, id: 5, name: 'Mejorar Drill 3 (+15)' },
+];
 
-export default function Clicker(){
+export default function Clicker() {
+
+    //Resources
     const [clickCounter, setClickCounter] = useState(0);
-    const [spillCounter, setSpillCounter] = useState(0);
+    const [oilLiters, setOilLiters] = useState(0);
+    const [fishesSpecimens, setFishesSpecimens] = useState(randomBetween(248, 369));
+
+    // Parameters
+    const [waterHealth, setWaterHealth] = useState(1);
+    const [litersPerClick, setLitersPerClick] = useState(1);
+    const [spillThreshold, setSpillThreshold] = useState(150);
+    const [spillProbability, setSpillProbability] = useState(0.2);
+    const [recoveryProbability, setRecoveryProbability] = useState(0.1);
+
+    // Helper states
     const [clicksSinceLastSpill, setClicksSinceLastSpill] = useState(Infinity);
-    const [oilLitters, setOilLitters] = useState(0);
-    const [threshold, setThreshold] = useState( randomBetween(100, 200) );
-    const [marineSpeciesArray, setMarineSpeciesArray] = useState([{ species: 'Peces', key: "fish", quantity: 334 }, { species: 'Tortugas', key: "turtle", quantity: 38 }, { species: 'Tiburones', key: "sharks", quantity: 24 }]);
+    const [clicksSinceLastRecovery, setClicksSinceLastRecovery] = useState(Infinity);
     const [oilDropsArray, setOilDropsArray] = useState([]);
+    const [playing, setPlaying] = useState(true);
 
-    function clickHandler(){
-        setOilLitters(oilLitters + 10);
-        setClickCounter((clickCounter + 1));
+    function clickHandler() {
+        if (!playing) return;
+
+        setOilLiters(oilLiters + litersPerClick);
+        setClickCounter(clickCounter + 1);
+
+        // Events
+        if (clickCounter > spillThreshold && Math.random() <= spillProbability) {
+            setClicksSinceLastSpill(0);
+            setWaterHealth(waterHealth - 20);
+            setSpillProbability(spillProbability + 0.2);
+        }
+        else {
+            setClicksSinceLastSpill(clicksSinceLastSpill + 1);
+        }
+
+        if (Math.random() > waterHealth) {
+            let newFishSpecimens = fishesSpecimens - randomBetween(3, 10);
+            newFishSpecimens = newFishSpecimens < 0 ? 0 : newFishSpecimens;
+            setFishesSpecimens(newFishSpecimens);
+
+            if (!newFishSpecimens) {
+                setPlaying(false);
+                alert("El ecosistema quedó devastado. No quedan más especimenes. Perdimos.")
+            }
+        }
+        if (Math.random() <= recoveryProbability && waterHealth >= 0.8) {
+            setFishesSpecimens(fishesSpecimens + randomBetween(2, 5));
+            setClicksSinceLastRecovery(0);
+        }
+        else{
+            setClicksSinceLastRecovery(clicksSinceLastRecovery + 1)
+        }
+        
+
+        actionDropAnimation();
+    }
+    function storeHandler(id: number) {
+        switch (id) {
+            case 0:
+                setOilLiters(oilLiters - store[id].price);
+                setWaterHealth(waterHealth + 30);
+                setRecoveryProbability(recoveryProbability + 20);
+                break;
+
+            case 1:
+                setOilLiters(oilLiters - store[id].price);
+                setSpillThreshold(spillThreshold + 150);
+                break;
+
+            case 2:
+                setOilLiters(oilLiters - store[id].price);
+                setSpillThreshold(spillThreshold + 500);
+                break;
+
+            case 3:
+                setOilLiters(oilLiters - store[id].price);
+                setLitersPerClick(litersPerClick + 1);
+                break;
+
+            case 4:
+                setOilLiters(oilLiters - store[id].price);
+                setLitersPerClick(litersPerClick + 5);
+                break;
+
+            case 5:
+                setOilLiters(oilLiters - store[id].price);
+                setLitersPerClick(litersPerClick + 15);
+                break;
+
+            default:
+                break;
+        }
+    }
+    function actionDropAnimation() {
         // @ts-ignore
-        setOilDropsArray([...oilDropsArray, <div key={`div-${clickCounter}`} className="absolute"><div className={`w-12 top-5 absolute motion-drop-0`}><img src={oilDrop}/></div><div className={`w-12 top-5 absolute motion-drop-1`}><img src={oilDrop}/></div><div className={`w-12 top-5 absolute motion-drop-2`}><img src={oilDrop}/></div><div className={`w-12 top-5 absolute motion-drop-3`}><img src={oilDrop}/></div></div>]);
+        setOilDropsArray([...oilDropsArray, <div key={`div-${clickCounter}`} className="absolute"><div className={`w-12 top-5 absolute motion-drop-0`}><img src={oilDrop1} /></div><div className={`w-12 top-5 absolute motion-drop-1`}><img src={oilDrop1} /></div><div className={`w-12 top-5 absolute motion-drop-2`}><img src={oilDrop1} /></div><div className={`w-12 top-5 absolute motion-drop-3`}><img src={oilDrop1} /></div></div>]);
 
-        setTimeout(()=>{
+        setTimeout(() => {
             // @ts-ignore
             setOilDropsArray(oilDropsArray.slice(4));
         }, 200);
-
-        setClicksSinceLastSpill(clicksSinceLastSpill + 1);
     }
-    useEffect(()=>{
-        if(oilLitters <= threshold) return;
-        // alert('Ocurrio un derrame');
 
-        let totalSpecimens = 0;
-        let newMarineSpeciesArray = marineSpeciesArray.map( (element) => {
-            let newQuantity = Math.floor(element.quantity* randomBetween( 60, 80 ) / 100);
-            totalSpecimens += newQuantity;
-
-            return { ...element, quantity:  newQuantity}; 
-        });
-
-        setMarineSpeciesArray( newMarineSpeciesArray );
-        setThreshold( randomBetween(threshold + 10, threshold + 50) );
-        setSpillCounter(spillCounter + 1);
-        setClicksSinceLastSpill(0);
-
-        if(!totalSpecimens) alert("Haz arrazado con el ecosistema. Perdimos.");
-    },[oilLitters]);
-
-
-    return(
+    return (
         <div className="relative w-full flex-grow justify-center mt-4 pb-4 overflow-hidden">
-            <div className="bg-ocean w-full h-full absolute" style={{zIndex: -10, filter: `brightness(${ (1-spillCounter*0.15) < 0.45? 0.45 : (1-spillCounter*0.15) })`}}></div>
-            <div className="max-w-7xl w-full flex flex-col items-center overflow-hidden mt-4">
+            <div className="bg-ocean w-full h-full absolute" style={{ zIndex: -10, filter: `brightness(${1 - (0.55 * (1 - waterHealth))})` }}></div>
+            <div className="relative max-w-7xl w-full flex flex-col items-center overflow-hidden mt-4">
                 <div className="flex flex-col items-center transform hover:scale-105 hover:cursor-pointer select-none">
                     <div onClick={clickHandler} className="w-48 mt-16">
-                        <img src={oilDrillFigure}/>
+                        <img src={oilDrillFigure} />
                     </div>
                     <span className="font-bold">¡EXTRAER!</span>
 
                     {
-                        oilDropsArray.map((i)=>(i))
+                        oilDropsArray.map((i) => (i))
                     }
                 </div>
 
                 <table className="bg-sky-300/50 text-center text-2xl mt-7 rounded-lg">
-                    <thead>
-                        <tr>
-                            <th className="px-4 py-1 border border-gray-500">Elemento</th>
-                            <th className="px-4 py-1 border border-gray-500">Cantidad</th>
-                        </tr>
-                    </thead>
-                   
                     <tbody>
                         <tr key={'oil'}>
-                            <td  className="px-4 py-1 border border-gray-500">Petroleo</td>
-                            <td  className="px-4 py-1 border border-gray-500">{oilLitters} L</td>
+                            <td className="px-4 py-1 border border-gray-500">
+                                <img className="w-12" src={oilDrop1} />
+                            </td>
+                            <td className="px-4 py-1 border border-gray-500">{oilLiters} L</td>
                         </tr>
-                        {
-                            marineSpeciesArray.map((element) => (
-                                <tr key={element.key}>
-                                    <td  className="px-4 py-1 border border-gray-500">{element.species}</td>
-                                    <td  className="px-4 py-1 border border-gray-500">{element.quantity}</td>
-                                </tr>
-                            ))
-                        }
+                        <tr key={'fishes'}>
+                            <td className="px-4 py-1 border border-gray-500">
+                                <img className="w-12" src={fish} />
+                            </td>
+                            <td className="px-4 py-1 border border-gray-500">{fishesSpecimens}</td>
+                        </tr>
                     </tbody>
                 </table>
 
+                <div className="bg-gray-100 bg-opacity-60 flex flex-col p-3 items-center rounded-xl border border-blue-500 mt-6 md:absolute md:right-3">
+                    <h2 className="font-bold mb-4">Tienda</h2>
+                    <div className="flex flex-col items-center gap-y-3">
+                        {
+                            store.map( (i)=>(
+                                <button disabled={oilLiters < i.price} onClick={()=>{storeHandler(i.id)}} className={`border border-black px-3 py-2 rounded-lg  transform ${oilLiters >= i.price? 'bg-blue-400 hover:scale-105 hover:font-bold' : 'bg-gray-200 '}`} key={`store-element-${i.id}`}>{`${i.name} - ${i.price}L`}</button>
+                            ))
+                        }
+                    </div>
+                </div>
+
                 {
-                    clicksSinceLastSpill < 2 && 
-                    <div className="bg-red-500 py-1 px-2 rounded-lg border border-black animate-bounce absolute ">
+                    clicksSinceLastSpill < 2 && clicksSinceLastRecovery > clicksSinceLastSpill &&
+                    <div className="bg-red-500 py-1 px-2 rounded-lg border border-black animate-bounce absolute mt-4">
                         <span className="font-bold ">
                             ¡OCURRIÓ UN DERRAME!
+                        </span>
+                    </div>
+                }
+                {
+                    clicksSinceLastRecovery < 2 && clicksSinceLastSpill > clicksSinceLastRecovery &&
+                    <div className="bg-green-500 py-1 px-2 rounded-lg border border-black animate-bounce absolute mt-4">
+                        <span className="font-bold ">
+                            EL MAR SE ESTÁ RECUPERANDO
                         </span>
                     </div>
                 }
